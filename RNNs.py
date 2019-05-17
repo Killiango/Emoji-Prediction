0ef0dea7322b5ba1e91cb595d9b3017ebda65536
Lukas Muttenthaler <LukasMut@users.noreply.github.com>
2019-05-17 21:12:06 +0200

    Updated notebook and neural nets

--- RNNs.py
+++ RNNs.py
@@ -3,77 +3,95 @@ import tensorflow as tf
 import keras
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Embedding, LSTM, GRU, TimeDistributed
+from keras.layers import Dense, Dropout, Embedding, LSTM, GRU, TimeDistributed, Flatten
+
+### IMPORTANT NOTE: Initialize the weights of the embedding layer with pre-trained word embeddings (e.g., word2vec, GloVe)
+### Trainable needs to be set to false, if we pass pre-trained word embeddings such as word2vec or GloVe
 
 class GRU_NET():
+
+    #embedding_dim = 300
     
-    embedding_dim = 300
-    
-     def __init__(self, vocab_size: int, hidden_units: int, n_features: int, n_timesteps, labels_dim: int, dropout = False, optimizer):
+    def __init__(self, vocab_size: int, hidden_units: int, n_features: int, embedding_matrix, n_labels: int, 
+        optimizer, dropout = False):
         self.vocab_size = vocab_size
         self.hidden_units = hidden_units
         self.n_features = n_features
-        self.n_timesteps = n_timesteps
-        self.labels_dim = labels_dim
-        self.loss = "sparse_categorical_crossentropy" # if we want to predict word vecs instead of labels, use cosine proximity
-        self.dropout = dropout
+        self.embedding_matrix = embedding_matrix
+        self.n_labels = n_labels
+        self.loss = "categorical_crossentropy" # if we want to predict emoji vecs instead of emoji labels, use cosine proximity
         self.optimizer = optimizer
+        self.dropout = dropout
         
         # 2 GRU hidden layers with n hidden units
         # 1 output layer with softmax as activation function (output = probability distribution over the labels)
         print('Build model...')
         self.model = Sequential()
-        self.model.add(Embedding(vocab_size, embedding_dim, input_shape = (n_samples, n_timesteps)))
-        self.model.add(GRU(hidden_units, activation='relu', recurrent_activation='hard_sigmoid', return_sequences = True))       
-        self.model.add(GRU(hidden_units, activation='relu', recurrent_activation='hard_sigmoid', return_sequences = True))
         
-        if self.dropout != False:
+        self.model.add(Embedding(vocab_size + 2, n_features, weights = [embedding_matrix], 
+                                 trainable = False, mask_zero = True))
+        
+        self.model.add(GRU(hidden_units, activation='relu', recurrent_activation='hard_sigmoid', 
+                           return_sequences = True))    
+        if self.dropout:
             self.model.add(Dropout(dropout))
         
-        self.model.add(TimeDistributed(Dense(self.labels_dim, activation = 'softmax')))
+        self.model.add(GRU(hidden_units, activation='relu', recurrent_activation='hard_sigmoid', 
+                           return_sequences = False))
+        
+        if self.dropout:
+            self.model.add(Dropout(dropout))
+        
+        #self.model.add(TimeDistributed(Dense(self.n_labels, activation = 'softmax')))
+        self.model.add(Dense(self.n_labels, activation = 'softmax'))
         self.model.compile(loss = self.loss, optimizer = self.optimizer, metrics = ['accuracy'])
                        
-    def fit(self, X_train, y_train, n_epochs, n_batches):
-        return self.model.fit(Xtrain, ytrain, epochs = n_epochs, batch_size = n_batches)
+    def fit(self, X_train, y_train, X_val, y_val,  n_epochs, n_batches):
+        return self.model.fit(X_train, y_train, validation_data = (X_val, y_val), epochs = n_epochs, batch_size = n_batches)
     
     def predict(self, X_test):
         return self.model.predict(X_test)
 
 class LSTM_NET():
-    
-    embedding_dim = 300
-    
-    def __init__(self, vocab_size: int, hidden_units: int, n_features: int, n_timesteps = None, n_labels: int, dropout = False, optimizer):
+
+    def __init__(self, vocab_size: int, hidden_units: int, n_features: int, embedding_matrix, n_labels: int, 
+        optimizer, dropout = False):
         self.vocab_size = vocab_size
         self.hidden_units = hidden_units
         self.n_features = n_features
-        self.n_timesteps = n_timesteps
-        self.labels_dim = labels_dim
-        self.loss = "sparse_categorical_crossentropy" # if we want to predict emoji vecs instead of emoji labels, use cosine proximity
-        self.dropout = dropout
+        self.embedding_matrix = embedding_matrix
+        self.n_labels = n_labels
+        self.loss = "categorical_crossentropy" # if we want to predict emoji vecs instead of emoji labels, use cosine proximity
         self.optimizer = optimizer
+        self.dropout = dropout
         
         # 2 LSTM hidden layers with n hidden units
         # 1 output layer with softmax as activation function (output = probability distribution over the labels)
         print('Build model...')
         self.model = Sequential()
-        ### EMBEDDING -> MASKING ZERO SET TO TRUE for variable timesteps
-        self.model.add(Embedding(vocab_size, embedding_dim, input_shape = (n_samples, n_timesteps)))
-        self.model.add(LSTM(hidden_units, activation = 'relu', recurrent_activation = 'hard_sigmoid', return_sequences = True))
+
+        self.model.add(Embedding(vocab_size + 2, n_features, weights = [embedding_matrix], 
+                                 trainable = False, mask_zero = True))
         
-        if self.dropout != False:
+
+        self.model.add(LSTM(hidden_units, activation = 'relu', recurrent_activation = 'hard_sigmoid',
+                            return_sequences = True))
+        
+        if self.dropout:
             self.model.add(Dropout(self.dropout))
         
-        self.model.add(LSTM(hidden_units, activation = 'relu', recurrent_activation = 'hard_sigmoid', return_sequences = True))
+        self.model.add(LSTM(hidden_units, activation = 'relu', recurrent_activation = 'hard_sigmoid', 
+                        return_sequences = False))
         
-        if self.dropout != False:
+        if self.dropout:
             self.model.add(Dropout(self.dropout))
         
-        self.model.add(TimeDistributed(Dense(self.labels_dim, activation = 'softmax')))
+        #self.model.add(Flatten())
+        self.model.add(TimeDistributed(Dense(self.n_labels, activation = 'softmax')))
         self.model.compile(loss = self.loss, optimizer = self.optimizer, metrics = ['accuracy'])
         
-    def fit(self, X_train, y_train, n_epochs, n_batches):
-        return self.model.fit(Xtrain, ytrain, epochs = n_epochs, batch_size = n_batches)
+    def fit(self, X_train, y_train, X_val, y_val, n_epochs, n_batches):
+        return self.model.fit(Xtrain, ytrain, validation_data = (X_val, y_val), epochs = n_epochs, batch_size = n_batches)
     
     def predict(self, X_test):
         return self.model.predict(X_test)
